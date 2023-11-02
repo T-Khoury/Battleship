01e64fa7656a2d2c30e90e08ed088cd4959015d6
T-Khoury <120677926+T-Khoury@users.noreply.github.com>
2023-11-02 11:41:16 -0400

    Create ship tests

---
+++ src/tests/ship.test.js
@@ -0,0 +1,26 @@
+import { Ship } from '../modules/ship';
+
+
+describe('Ship functions', () => {
+    let submarine = Ship(3);
+
+    test('Ship with no hits returns false isSunk', () => {
+        expect(submarine.isSunk()).toBe(false)
+    })
+
+    test('hit function increments hits by 1', () => {
+        submarine.hit();
+        expect(submarine.hitNumber).toEqual(1)
+    })
+
+    test('Ship with less than length number of hits returns false isSunk', () => {
+        submarine.hit();
+        expect(submarine.isSunk()).toBe(false)
+    })
+
+    test('Ship with n length and n hits returns true isSunk', () => {
+        submarine.hit();
+        expect(submarine.isSunk()).toBe(true)
+    })
+})
+
