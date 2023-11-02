87cbe6ca31a647ca1b30ae1533754a1fd1c83849
T-Khoury <120677926+T-Khoury@users.noreply.github.com>
2023-11-02 07:13:38 -0400

    Create Ship factory

---
+++ src/modules/ship.js
@@ -0,0 +1,21 @@
+function Ship(length) {
+    let ship = {
+        length,
+        hitNumber: 0,
+        sunk: false,
+        hit() {
+            this.hitNumber ++
+        },
+        isSunk() {
+            if (this.length === this.hitNumber) {
+                this.sunk = true
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+    return ship
+}
+
+export { Ship }
